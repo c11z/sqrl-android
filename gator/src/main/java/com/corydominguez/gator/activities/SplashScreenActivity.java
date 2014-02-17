7dc1bd4f18654127b9810d7ec14c2a3c0f668830
Jarrett Coggin <jcoggin@yahoo-inc.com>
2014-02-16 22:24:10 -0800

    fixing the missing action bar

--- gator/src/main/java/com/corydominguez/gator/activities/SplashScreenActivity.java
+++ gator/src/main/java/com/corydominguez/gator/activities/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.corydominguez.gator.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +23,8 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ActionBar ab = getActionBar();
+        ab.hide();
  
         new Handler().postDelayed(new Runnable() {
  

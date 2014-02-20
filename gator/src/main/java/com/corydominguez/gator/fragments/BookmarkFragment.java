ffa3f5322bdc9b6600d02d02b903644de2c842d9
Cory Dominguez <corydominguez@gmail.com>
2014-02-19 20:46:47 -0800

    added non functioning tabs and did some styling

---
+++ gator/src/main/java/com/corydominguez/gator/fragments/BookmarkFragment.java
@@ -0,0 +1,7 @@
+package com.corydominguez.gator.fragments;
+
+/**
+ * Created by coryd on 19/02/2014.
+ */
+public class BookmarkFragment extends LinkListFragment {
+}
---
+++ gator/src/main/java/com/corydominguez/gator/fragments/HomeFragment.java
@@ -0,0 +1,9 @@
+package com.corydominguez.gator.fragments;
+
+/**
+ * Created by coryd on 19/02/2014.
+ */
+public class HomeFragment extends LinkListFragment {
+
+
+}

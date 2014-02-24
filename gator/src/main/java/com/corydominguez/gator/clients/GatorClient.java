8e56f843a76860b28b2c10f1f33e314eee67a011
Jarrett Coggin <jcoggin@yahoo-inc.com>
2014-02-24 13:26:50 -0800

    reconfiguring server to work with AWS instance.

--- gator/src/main/java/com/corydominguez/gator/clients/GatorClient.java
+++ gator/src/main/java/com/corydominguez/gator/clients/GatorClient.java
@@ -8,9 +8,15 @@ import com.corydominguez.gator.handlers.GatorHttpHandler;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import org.apache.http.message.BasicNameValuePair;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.List;
 
 //import com.corydominguez.gator.models.Tweet;
 
@@ -19,7 +25,7 @@ import java.util.Calendar;
  * Created by coryd on 2/13/14.
  */
 public class GatorClient extends AsyncHttpClient {
-    public static final String REST_URL = "http://lickstick.corp.ne1.yahoo.com";
+    public static final String REST_URL = "http://ec2-54-84-52-116.compute-1.amazonaws.com";
     public static final String TWEETER_ID = "164865735";
     private DateFormat df = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
     private GatorHttpHandler handler;
@@ -38,18 +44,40 @@ public class GatorClient extends AsyncHttpClient {
 
     public void getPast24() {
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, -2);
+        cal.add(Calendar.DATE, -1);
         String yesterday = df.format(cal.getTime());
-        String url = getApiUrl("linkbundle", yesterday);
+        ArrayList<BasicNameValuePair> params = new ArrayList<BasicNameValuePair>();
+        params.add(new BasicNameValuePair("startDate", yesterday.split(" ")[0]));
+        String url = getApiUrlWithParams("linkbundle", params);
         handler.appendMode = false;
         get(url, handler);
     }
 
+    private String getApiUrlWithParams(String endpoint, List<BasicNameValuePair> params) {
+        String url = REST_URL + "/" + endpoint;
+        String paramsAddition = "";
+        for(int i = 0; i < params.size(); i++){
+            BasicNameValuePair param = params.get(i);
+            paramsAddition += param.getName() + "=" +param.getValue();
+            if(i+1 < params.size()){
+                paramsAddition += "&";
+            }
+        }
+        if(paramsAddition!= ""){
+            url += "?" + paramsAddition;
+        }
+        return url;
+    }
+
     private String getApiUrl(String endpoint, String since) {
-        return REST_URL + "/" + endpoint + "/" + TWEETER_ID + "/" + Uri.encode(since);
+        //OLD URL: REST_URL + "/" + endpoint + "/" + TWEETER_ID + "/" + Uri.encode(since);
+        String url = REST_URL + "/" + endpoint + "/" + Uri.encode(since.split(" ")[0]);
+        return url;
     }
 
     private String getApiUrl(String endpoint, String tweeterId, String since) {
-        return REST_URL + "/" + endpoint + "/" + tweeterId + "/" + Uri.encode(since);
+        //OLD URL: REST_URL + "/" + endpoint + "/" + tweeterId + "/" + Uri.encode(since);
+        String url = REST_URL + "/" + endpoint + "/" + tweeterId + "/" + Uri.encode(since);
+        return url;
     }
 }
